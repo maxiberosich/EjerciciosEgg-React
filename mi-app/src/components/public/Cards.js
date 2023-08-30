5669e28a2a9cd80f3035f31c63eb82673ae3599c
maxiberosich <mberosich@gmail.com>
2023-08-30 19:23:01 -0300

    Agregado logo al header

---
+++ mi-app/src/components/public/Cards.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from './Card'
+
+export const Cards = () => {
+  return (
+    <div className="album py-5 bg-body-tertiary">
+          <div className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <Card/>
+            </div>
+          </div>
+        </div>
+  )
+}
--- mi-app/src/components/public/Navbar.js
+++ mi-app/src/components/public/Navbar.js
@@ -5,6 +5,12 @@ export const Navbar = () => {
     <header className="p-3 text-bg-dark">
         <div className="container">
           <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+            <span
+            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+              <img className='App-logo' height="52" src='LogoMCB.png' alt="Logo de la empresa"></img>
+            </span>
+
             <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li>
                 <a href="#" className="nav-link px-2 text-secondary">
