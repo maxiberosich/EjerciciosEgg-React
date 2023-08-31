d84f5b4a891be9aadfbe850b15a2297e16a2b249
maxiberosich <mberosich@gmail.com>
2023-08-31 20:58:11 -0300

    Creacion del react-router-dom

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import {Footer} from "./components/public/Footer";
 import Main from "./components/public/Main";
 import {Navbar} from "./components/public/Navbar";
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { UserForm } from './components/public/UserForm';
+import { Detail } from './components/public/Detail';
 
 function App() {
   return (
     <div>
       
       <Navbar />
-      <Main/>      
+      <Routes>
+        <Route exact path={"/"} element={<Main/>} />
+        <Route path={"/details/:id"} element={<Detail/>} />
+        <Route path={"/user-form"} element={<UserForm/>} />
+      </Routes>   
       <Footer />
 
 
---
+++ mi-app/src/components/public/Detail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Detail = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
