d4b38d01a2f7207f989a4240a47f41a7cbec7364
maxiberosich <mberosich@gmail.com>
2023-09-03 17:44:24 -0300

    Modificacion formulario

--- mi-app/src/components/public/UserForm.js
+++ mi-app/src/components/public/UserForm.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useUserForm } from "../../hooks/Custom.hooks";
+import { useState } from "react";
 
 export const UserForm = () => {
+  const { form, setForm, handleChanges } = useUserForm();
 
-  const {form,setForm,handleChanges} = useUserForm();
+  const [areEquals, setAreEquals] = useState(true);
+  const [passwordConfirmation, setPasswordConfirmation] = useState();
+
+  const handlePasswordConfirmation = (e) =>{
+    setPasswordConfirmation(e.target.value);
+  }
+
+  useEffect(()=>{
+
+    const {password} = form;//EL PASSWORD HACE REFERENCIA AL DEL NAME DE CLAVE
+    setAreEquals(password === passwordConfirmation)
+
+  },[passwordConfirmation])
+
+  const handleSubmit = (evento) =>{
+    evento.preventDefault();
+  }
 
   return (
     <div className="w-25 mx-auto mt-5">
@@ -19,6 +37,8 @@ export const UserForm = () => {
               type="email"
               className="form-control"
               placeholder="you@example.com"
+              name="email"
+              onChange={handleChanges}
             />
           </div>
 
@@ -26,7 +46,12 @@ export const UserForm = () => {
             <label htmlFor="address" className="form-label">
               Clave
             </label>
-            <input type="text" className="form-control" placeholder="Clave" />
+            <input 
+            type="text" className="form-control" placeholder="Clave"
+            name="password"
+            onChange={handleChanges}
+            />
+            
           </div>
 
           <div className="col-12">
@@ -37,16 +62,23 @@ export const UserForm = () => {
               type="text"
               className="form-control"
               placeholder="Repetir clave"
+              onChange={handlePasswordConfirmation}
             />
-          </div>
 
-          <div className="form-text list-group-item-danger">
-            Las Claves no son iguales
+            {!areEquals && (
+            <div className="form-text list-group-item list-group-item-danger">
+              Las Claves no son iguales
+            </div>
+            )}
           </div>
 
           <hr className="my-4" />
 
-          <button className="m-100 btn btn-primary btn-lg" type="submit">
+          <button 
+          disabled={!areEquals}
+          className="m-100 btn btn-primary btn-lg" type="submit"
+          onClick={handleSubmit}
+          >
             Registrarme
           </button>
         </div>
